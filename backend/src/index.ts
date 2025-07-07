80d7b843710e429b2c01796162345706617201c5
TristanCC <tristan.c.johnston@gmail.com>
2025-07-07 02:30:41 -0500

    implemented basic upload functionality!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
+import multer from "multer"
 
 const app = express()
 const port = process.env.PORT
+
+// setup multer config for defining file storage location
+
+const upload = multer({dest: 'uploads/'})
 
 app.use((req, res, next) => {
     console.log("i am middleware!")
@@ -12,6 +17,16 @@ app.use((req, res, next) => {
 app.get("/", (req,res) => {
     console.log("hello world!")
     res.send("hello world!")
+})
+
+app.post(`/uploadPDF`, upload.single('uploadedPDF') ,(req, res) => {
+    const pdf = req.file
+    if(pdf) {
+        res.status(200).json("recieved pdf!")
+    }
+    else {
+        res.status(500).json("internal server error.")
+    }
 })
 
 
---
+++ backend/src/utils.ts
@@ -0,0 +1,3 @@
+//const fileFilter = (file) => {
+//    
+//}
